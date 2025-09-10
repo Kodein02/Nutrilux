6d6592e976184e45070429bfcaa543341e742215
Pataki Attila Bence <126593297+Kodein02@users.noreply.github.com>
2025-09-10 21:42:46 +0000

    add root route

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Nutrilux server running 🚀');
+    res.send('Nutrilux server running 🚀');
 });
 app.use(express.json());
 
-app.post('/webhook/seal', (req, res) => {
+app.post("/webhooks/seal", (req, res) => {
     console.log("Webhook received:", req.body);
-    res.sendStatus(200);
+    res.status(200).send("ok");
 });
 
 app.listen(3000, () => console.log("Running on port 3000"));
-app.get('/', (req, res) => {
-  res.send('Nutrilux server running 🚀');
-});
