a3b5f3b082d04ca850d800ce3094da495af85b20
Pataki Attila Bence <126593297+Kodein02@users.noreply.github.com>
2025-09-10 21:46:47 +0000

    use process.env.PORT on Render

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ app.post("/webhooks/seal", (req, res) => {
     res.status(200).send("ok");
 });
 
-app.listen(3000, () => console.log("Running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
